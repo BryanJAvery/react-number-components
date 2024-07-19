bd36b36d2e523af514b555145547d7c524cf5c15
BryanJAvery <freelancerupwork822@gmail.com>
2024-07-19 13:40:00 -0400

    react-number-components

---
+++ rollup.config.js
@@ -0,0 +1,81 @@
+import { uglify } from 'rollup-plugin-uglify';
+import fileSize from 'rollup-plugin-filesize';
+import license from 'rollup-plugin-license';
+import replace from 'rollup-plugin-replace';
+import commonjs from 'rollup-plugin-commonjs';
+import buble from '@rollup/plugin-buble';
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+import PACKAGE from './package.json';
+const fullYear = new Date().getFullYear();
+
+const banner = `${PACKAGE.name} - ${PACKAGE.version}
+  Author : ${PACKAGE.author}
+  Copyright (c) ${fullYear !== 2016 ? '2016,' : ''} ${fullYear} to ${
+  PACKAGE.author
+}, released under the ${PACKAGE.license} license.
+  ${PACKAGE.repository.url}`;
+
+const globals = {
+  react: 'React',
+};
+
+const defaultConfig = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/react-number-format.es.js',
+      format: 'esm',
+      globals,
+      exports: 'auto',
+    },
+    {
+      file: 'dist/react-number-format.cjs.js',
+      format: 'cjs',
+      globals,
+      exports: 'auto',
+    },
+    {
+      file: 'dist/react-number-format.js',
+      format: 'umd',
+      name: 'NumberFormat',
+      globals,
+      exports: 'auto',
+    },
+  ],
+  external: ['react'],
+  plugins: [
+    typescript({
+      target: 'es2016',
+    }),
+    buble({
+      objectAssign: true,
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    resolve(),
+    commonjs({
+      include: /node_modules/,
+    }),
+    fileSize(),
+    license({
+      banner,
+    }),
+  ],
+};
+
+const minConfig = {
+  ...defaultConfig,
+  output: {
+    file: 'dist/react-number-format.min.js',
+    format: 'umd',
+    name: 'NumberFormat',
+    globals,
+    exports: 'auto',
+  },
+  plugins: [...defaultConfig.plugins, uglify()],
+};
+
+export default [defaultConfig, minConfig];
